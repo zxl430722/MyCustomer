c2dd3538988b2f4f68a323bb3ef98062e61b5b48
曾晓龙大大 <zengxiaolong430722@163.com>
2017-08-11 16:56:02 +0800

    新增Satellite

--- app/src/main/java/com/zxl/mycustomer/MainActivity.java
+++ app/src/main/java/com/zxl/mycustomer/MainActivity.java
@@ -1,13 +1,53 @@
 package com.zxl.mycustomer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.zxl.mycustomer.satellite.SatelliteMenu;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private SatelliteMenu satelliteMenu;
+    private Button btn_set_position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        satelliteMenu= (SatelliteMenu) findViewById(R.id.sm_menu);
+        satelliteMenu.setOnMenuItemClickListener(new SatelliteMenu.onMenuItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(MainActivity.this, view.getTag().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn_set_position= (Button) findViewById(R.id.btn_set_position);
+        btn_set_position.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rand=new Random();
+                int position=rand.nextInt(3);
+                switch (position) {
+                    case 0:
+                        satelliteMenu.setPosition(SatelliteMenu.Position.POS_LEFT_TOP);
+                        break;
+
+                    case 1:
+                        satelliteMenu.setPosition(SatelliteMenu.Position.POS_LEFT_BOTTOM);
+                        break;
+                    case 2:
+                        satelliteMenu.setPosition(SatelliteMenu.Position.POS_RIGHT_TOP);
+                        break;
+                    case 3:
+                        satelliteMenu.setPosition(SatelliteMenu.Position.POS_RIGHT_BOTTOM);
+                        break;
+                }
+
+            }
+        });
     }
 }
