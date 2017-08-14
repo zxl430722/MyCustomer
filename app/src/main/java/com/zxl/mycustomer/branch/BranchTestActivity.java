12bcbd369e0c32074d319dbd8019c6b3c5a4de67
曾晓龙大大 <zengxiaolong430722@163.com>
2017-08-14 15:35:43 +0800

    branch test

---
+++ app/src/main/java/com/zxl/mycustomer/branch/BranchTestActivity.java
@@ -0,0 +1,20 @@
+package com.zxl.mycustomer.branch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zxl.mycustomer.R;
+
+/**
+ * author: ZXL
+ * created on: 2017/8/14 15:29
+ * description:
+ */
+
+public class BranchTestActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_branch_test_layout);
+    }
+}
